54bf0dbfeb5cec1e198efc665e9fc97f2681f081
Bill Klinton <bklinton14@gmail.com>
2022-07-07 09:35:25 -0300

    header fix, profile and experiences component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ExperiencesComponent } from './experiences/experiences.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent
+    ProfileComponent,
+    ExperiencesComponent
   ],
   imports: [
     BrowserModule,
